e0e46ca4c863a149b76ac5d1b4fb580c1f34f20c
t-onystar-k <56377023+t-onystar-k@users.noreply.github.com>
2021-02-18 17:10:38 +0530

    Delete HealthTracker directory

--- HealthTracker/HealthTracker/Form2.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class Form2
-    Public Property userId As String
-
-    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Label4.Text = userId
-
-    End Sub
-End Class
--- HealthTracker/HealthTracker/Form3.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class Form3
-    Public Property userId As String
-
-    Private Sub Form3_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Label4.Text = userId
-
-    End Sub
-End Class
